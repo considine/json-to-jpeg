f5aa011ab30f7b2b88c431f08b521171ab3d873c
John P Considine <jconsidi@nd.edu>
2017-08-20 21:49:33 -0500

    first commit. Script that takes input as specified in table-data, and creates a graph image

--- index.js
+++ index.js
@@ -11,14 +11,12 @@ var wordLengthUtils = require("./lib/wordlengthutils.js");
 // Get data
 const DATA_TABLE = require("./table-data.js");
 var DataController = require("./lib/table-controller");
-
+wordLengthUtils.columnCheck(DATA_TABLE);
 var dc = new DataController(DATA_TABLE, false, CanvasConfig.contextWrapper);
-dc.writeRow();
-dc.writeRow();
-dc.writeRow();
-dc.writeRow();
-dc.writeRow();
+dc.writeRows();
+dc.printImage();
 
-console.log("<img src='" + CanvasConfig.canvas.toDataURL() + "'>");
+
+// console.log("<img src='" + CanvasConfig.canvas.toDataURL() + "'>");
 
 wordLengthUtils.columnCheck(DATA_TABLE);
